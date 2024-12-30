ad3559ba0f574db9c8b51545ef4d32da2897c824
novaldanorel <danorel048@gmail.com>
2024-12-30 10:08:27 +0700

    menambahkan folder router

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,18 @@ import HeroPage from './Component/HeroPage/Hero';
 import About from './Component/AboutPage/About';
 import Portfolio from './Component/PortfolioPage/Portfolio';
 import Client from './Component/ClientPage/Client';
-import Blog from './Component/BlogPage/Blog';
+import Blog from './Component/BLogPage/Blog';
 import Contact from './Component/ContactPage/Contact';
 import Footer from './Component/Footer/Footer';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import BlogPages from './router/BlogPage/BlogPages';
+import BlogPages from './router/Blog/BlogPages';
 import NotFound from './router/NotFound/NotFound';
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* Rute untuk halaman statis lainnya */}
         <Route
           path="/"
           element={
@@ -33,7 +32,6 @@ function App() {
           }
         />
 
-        {/* Rute untuk BlogPages */}
         <Route path="/Blog" element={<BlogPages />} />
         <Route path="*" element={<NotFound/>} />
       </Routes>
